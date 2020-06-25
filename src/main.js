e9fa213823dd66ab9e5120f6818aae370366fb74
bazsup <supawit.ruen@mail.kmutt.ac.th>
2020-06-25 15:47:26 +0700

    [EDIT] show app view instead of matrixui

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
-// import App from './App.vue'
-import MatrixUi from '@/MatrixUi.vue'
+import App from './App.vue'
+// import MatrixUi from '@/MatrixUi.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(MatrixUi),
+  render: h => h(App),
 }).$mount('#app')
