521807c023e014ca2d33e95a65c37fd652b5e7f1
bazsup <supawit.ruen@mail.kmutt.ac.th>
2020-06-25 15:18:18 +0700

    [EDIT] change style Room component using Bulma

--- src/components/Room.vue
+++ src/components/Room.vue
@@ -1,18 +1,21 @@
 <template>
   <div>
-    <h1>Room list</h1>
-    <ul>
-      <li :style="`color: ${activeRoom === room.roomId ? 'blue' : 'black'}`" v-for="room in rooms" :key="room.roomId" @click="selectRoom(room)">
-        name: {{room.name}} ->
+    <nav class="panel">
+      <p class="panel-heading">
+        Room list
+      </p>
+      <a class="panel-block" :class="{ 'is-active': activeRoom === room.roomId, 'has-text-info': activeRoom === room.roomId }" v-for="room in rooms" :key="room.roomId" @click="selectRoom(room)">
+        <span class="panel-icon">
+          <i class="fas fa-arrow-alt-circle-right" aria-hidden="true"></i>
+        </span>
+        {{ room.name }} ->
         {{ isDirectMessage(room) ? 'direct room' : 'group room' }}
-      </li>
-    </ul>
+      </a>
+    </nav>
   </div>
 </template>
 
 <script>
-
-
 export default {
   data() {
     return {
